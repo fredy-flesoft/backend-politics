4065224785c528f5799f73a31eba06ed5ddab6f5
junior <junior@fle.pe>
2026-04-09 12:09:31 -0500

    backend-mi comits

---
+++ src/main/java/com/example/backend_politics/BackendPoliticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_politics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendPoliticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendPoliticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backend_politics/BackendPoliticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_politics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendPoliticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
